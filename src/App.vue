fa08ae789de5c9d48b6437ae3d3cb81281297950
jjammar9 <jjam24306@gmail.com>
2026-05-19 13:22:51 +0200

    layout display

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <h1>Taska is alive 🚀</h1>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import TasksView from "../views/TasksView.vue";
+import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/tasks",
+      name: "tasks",
+      component: TasksView,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfileView,
+    },
+  ],
+});
 
-export default router
+export default router;
